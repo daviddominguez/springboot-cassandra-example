dec219544b2a0c28aec8ee036794c81b2db567e5
David Dominguez <david.dominguez@amplia.es>
2016-08-25 12:40:43 +0200

    feat(springboot-cassandra-example): add main class, banner and logging conf

---
+++ src/main/java/es/amplia/cassandra/SpringBootCassandraApplication.java
@@ -0,0 +1,18 @@
+package es.amplia.cassandra;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
+
+@SpringBootApplication(exclude = {WebClientAutoConfiguration.class})
+public class SpringBootCassandraApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCassandraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws InterruptedException {
+    }
+}
